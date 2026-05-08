31ab05c5b6d0397b40b2fd39f64d07a1a0fad2dc
Roshan Mahato <roshankumarmahato1312@gmail.com>
2026-05-08 06:46:29 +0530

    backend web service connected to frontend

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken } from "./auth";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:5000/api";
+const baseURL = process.env.NEXT_PUBLIC_API_URL ?? "https://dev-collab-backend-3.onrender.com/api";
 
 export const api = axios.create({
   baseURL
